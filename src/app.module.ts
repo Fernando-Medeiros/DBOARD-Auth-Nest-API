b88f88871d20dbbc7adb3196abeefe553093702e
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-21 20:33:24 -0300

    feat: app module -> add imports new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CustomerModule } from './customer/customer.module';
+import { PasswordModule } from './password/password.module';
 
 @Module({
-    imports: [],
+    imports: [AuthModule, CustomerModule, PasswordModule],
     controllers: [AppController],
     providers: [AppService],
 })
