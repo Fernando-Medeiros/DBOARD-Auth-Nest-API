7d16e689d7abc5033b78028e83d1078d4c0901f3
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-21 21:20:47 -0300

    feat: add modules - providers -> PrismaService

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
 
 @Module({
     controllers: [CustomerController],
-    providers: [CustomerService],
+    providers: [PrismaService, CustomerService],
 })
 export class CustomerModule {}
--- src/password/password.module.ts
+++ src/password/password.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { PasswordService } from './password.service';
 import { PasswordController } from './password.controller';
 
 @Module({
     controllers: [PasswordController],
-    providers: [PasswordService],
+    providers: [PrismaService, PasswordService],
 })
 export class PasswordModule {}
