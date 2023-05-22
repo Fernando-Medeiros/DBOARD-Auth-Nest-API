df812a7d9187304cfaa68107b432b2622623d066
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-21 21:01:24 -0300

    feat: Swagger Config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerConfig } from './swagger/swagger.module.config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    new SwaggerConfig(app).setup();
+
     await app.listen(3000);
 }
 bootstrap();
---
+++ src/swagger/swagger.module.config.ts
@@ -0,0 +1,32 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export class SwaggerConfig {
+    readonly version = '1.0';
+    readonly title = 'Dashboard-Auth-Nest-API';
+    readonly description = '';
+    readonly path = '/docs';
+    readonly contact = {
+        name: 'Fernando-Medeiros',
+        url: 'https://github.com/Fernando-Medeiros',
+        email: 'fernandomedeiros3k@gmail.com',
+    };
+
+    constructor(private app: INestApplication) {}
+
+    document() {
+        return SwaggerModule.createDocument(
+            this.app,
+            new DocumentBuilder()
+                .setTitle(this.title)
+                .setDescription(this.description)
+                .setVersion(this.version)
+                .setContact(this.contact.name, this.contact.url, this.contact.email)
+                .build(),
+        );
+    }
+
+    setup() {
+        SwaggerModule.setup(this.path, this.app, this.document());
+    }
+}
