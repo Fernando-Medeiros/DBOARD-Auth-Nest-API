5027042d7781151fc9bd41b64d559d8a24759428
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-21 21:56:12 -0300

    feat: useGlobalInterceptors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerConfig } from './swagger/swagger.module.config';
+import BaseInterceptor from './common/errors/interceptors/BaseInterceptor';
+import { notFoundInterceptor } from './common/errors/interceptors/not-found.interceptor';
+import { badRequestInterceptor } from './common/errors/interceptors/bad-request.interceptor';
+import { unauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -16,6 +20,12 @@ async function bootstrap() {
         }),
     );
 
-    await app.listen(3000);
+    app.useGlobalInterceptors(
+        (new BaseInterceptor(notFoundInterceptor),
+        new BaseInterceptor(badRequestInterceptor),
+        new BaseInterceptor(unauthorizedInterceptor)),
+    );
+
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
