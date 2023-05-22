ec9671a5ac63e9064f6aeb5d9c63c3eb177f72d6
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-21 22:32:26 -0300

    feat: Customer -> impl I.O.

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCustomerDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCustomerDto {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,1 +1,10 @@
-export class Customer {}
+import { Customer } from '@prisma/client';
+
+export class CustomerEntity implements Customer {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+}
