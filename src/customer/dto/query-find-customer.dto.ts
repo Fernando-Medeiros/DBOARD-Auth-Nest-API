9d42f3aef88bad39a61a9ba9483d6deed4d0c678
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-24 14:15:55 -0300

    feat: impl query-find-customer-dto

--- src/customer/dto/query-find-customer.dto.ts
+++ src/customer/dto/query-find-customer.dto.ts
@@ -1,7 +1,22 @@
-export class QueryFindCustomer {
-    limit?: string;
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
+export class QueryFindCustomer {
+    @IsString()
+    @IsOptional()
+    limit?: string = '10';
+
+    @IsString()
+    @IsOptional()
+    @IsEnum({ asc: 'asc', desc: 'desc' })
     order?: 'asc' | 'desc' = 'asc';
 
-    sort?: 'id' | 'email' | 'firstName' | 'lastName' = 'id';
+    @IsOptional()
+    @IsEnum({
+        id: 'id',
+        email: 'email',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        createdAt: 'createdAt',
+    })
+    sort?: 'id' | 'email' | 'firstName' | 'lastName' | 'createdAt' = 'createdAt';
 }
