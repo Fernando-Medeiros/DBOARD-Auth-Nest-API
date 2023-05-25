d485ad573714e080700951280cf80f77d4bcd5e9
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-24 21:25:03 -0300

    refactor: UnauthorizedInterceptor -> message

--- src/common/errors/interceptors/unauthorized.interceptor.ts
+++ src/common/errors/interceptors/unauthorized.interceptor.ts
@@ -5,7 +5,9 @@ import UnauthorizedError from 'errors/UnauthorizedError';
 export class UnauthorizedInterceptor extends BaseInterceptor {
     callback(error: any) {
         if (error instanceof UnauthorizedError) {
-            throw new UnauthorizedException(error.message);
+            throw new UnauthorizedException(
+                'Unauthorized access, check credentials or login again',
+            );
         }
         throw error;
     }
