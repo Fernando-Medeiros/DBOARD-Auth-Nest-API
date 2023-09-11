5d921fb3a31768273f90fc43747493e12745ecc1
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-11 18:09:08 -0300

    feat[module][customer]: Swagger Docs

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,10 +1,18 @@
-import { Customer } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { Customer as CustomerEntity } from '@prisma/client';
 
-export class CustomerEntity implements Customer {
+export class Customer implements CustomerEntity {
+    @ApiProperty()
     id: string;
+    @ApiProperty()
     firstName: string;
+    @ApiProperty()
     lastName: string;
+    @ApiProperty()
     email: string;
+
     password: string;
+
+    @ApiProperty()
     createdAt: Date;
 }
